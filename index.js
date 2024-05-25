c736fdff7b002279f3c000057d6e70c59ce4b65d
snomiao <snomiao@gmail.com>
2024-05-25 17:00:13 +0000

    fix(master): ignore prettier
    ignore prettier

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { render } from 'react-dom';
-import FimathCalc from './fimathCalc';
+import React from "react";
+import { render } from "react-dom";
+import FimathCalc from "./fimathCalc";
 
-const App = ()=> (<FimathCalc/>)
-render(<App />, document.getElementById('root'));
+const App = () => <FimathCalc />;
+render(<App />, document.getElementById("root"));
